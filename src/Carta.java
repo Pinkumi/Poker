101e799d659d2e9ca70d44360c4897ed0d136e7f
LaptopYael <yaellimon060905@gmail.com>
2025-05-13 19:17:06 -0700

    imagenes de cartas

--- src/Carta.java
+++ src/Carta.java
@@ -1,7 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Carta  extends JLabel {
     //clase carta
+    private int valor;
+    private String figura;
 
+    public Carta(int valor, String figura ) {
+        this.valor = valor;
+        this.figura = figura;
+        setOpaque(true);
+        setBackground(Color.WHITE);
+        setBounds(0,0,100,300);
+
+
+    }
+
+
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getFigura() {
+        return figura;
+    }
 
 }
