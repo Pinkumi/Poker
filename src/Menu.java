6dc999cd823821010a731b7d77b9b925dda987f2
LaptopYael <yaellimon060905@gmail.com>
2025-05-16 12:17:06 -0700

    Correccion de Merge

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,8 @@
 public class Menu {
     public static void main(String[] args) {
+
         FiveCardDraw fiveCardDraw = new FiveCardDraw();
+
+
     }
 }
--- src/SevenCardStud.java
+++ src/SevenCardStud.java
@@ -1,7 +1,8 @@
 public class SevenCardStud extends Poker{
-        //segunda modalidad de juego (subclase de Poker)
+    //segunda modalidad de juego (subclase de Poker)
     public SevenCardStud(){
-
+        super();
+        setTitle("Seven Card Stud");
     }
 
     @Override
